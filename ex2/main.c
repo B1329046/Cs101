65cc08369d4b3e5c9667387c27ad2ce9c9fab78a
B1329046 <sunnyhsieh0827@gmail.com>
2024-11-28 13:50:01 +0800

    Update main.c

--- ex2/main.c
+++ ex2/main.c
@@ -1,1 +1,12 @@
+#include <studio.h>
 
+int main(){
+  print("  November 2024\n");
+print(" Mo Tu We Th Fr  Sa  Su\n");
+print("                  1  2\n");
+print("  3  4  5  6  7   8  9\n");
+print(" 10 11 12 13 14  15 16\n");
+print(" 17 18 19 20 21  22 23\n");
+print(" 24 25 26 27 28  29 30\n");
+return 0;
+}
