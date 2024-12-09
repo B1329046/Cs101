3084df7b4b589323da277ebfb3025c9397af597c
B1329046 <sunnyhsieh0827@gmail.com>
2024-12-09 20:16:04 +0800

    Update main1.c

--- Hw5/main1.c
+++ Hw5/main1.c
@@ -1,3 +1,34 @@
-#include <studio.h>
+#include <stdio.h>
+
 int main() {
+    int n = 7;
+    int idx = 7;
+    int number = 0;
+
+    for ( int i = 1 ; i <= n ; i++){
+        number = 0;
+        for(int k = 0 ; k<= n*2 ; k++){
+           if((k == idx)||(k == idx+ number*2)){
+               printf("%d" , i);
+               number ++;
+               if (number == i){
+                    break;
+               }
+             
+           }
+           
+           else{
+               printf(" ");
+           }
+        }   
+        
+        printf("\n");
+        idx -- ;
+    }
+
+    
+
+    return 0;
+}
+
     
